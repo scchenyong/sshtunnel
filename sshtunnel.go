18e0329caf77d858a551659195b6f056126c158e
chenyong <scchenyong@189.cn>
2017-09-14 14:43:24 +0800

    调整结构，完善日志记录

--- sshtunnel.go
+++ sshtunnel.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net"
 	"os"
 	"sync"
-	"time"
 
 	"golang.org/x/crypto/ssh"
 )
@@ -20,17 +18,80 @@ type Tunnel struct {
 }
 
 type SSHTunnel struct {
-	Addr    string
-	User    string
-	Pass    string
-	Tunnels []Tunnel
+	sshClient *ssh.Client
+	Addr      string
+	User      string
+	Pass      string
+	Tunnels   []Tunnel
 }
 
-var sts []SSHTunnel
+func (st *SSHTunnel) Close() {
+	if nil != st.sshClient {
+		st.sshClient.Close()
+	}
+}
 
-func init() {
+func (st *SSHTunnel) Client() (*ssh.Client, error) {
+	if st.sshClient != nil {
+		return st.sshClient, nil
+	}
+	var auth []ssh.AuthMethod
+	auth = make([]ssh.AuthMethod, 0)
+	auth = append(auth, ssh.Password(st.Pass))
+
+	sc := &ssh.ClientConfig{
+		User: st.User,
+		Auth: auth,
+		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			return nil
+		},
+	}
+	var err error
+	st.sshClient, err = ssh.Dial("tcp", st.Addr, sc)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("连接到服务器成功: %s", st.Addr)
+	return st.sshClient, err
+}
+
+func (st *SSHTunnel) connect(t Tunnel) {
+	ll, err := net.Listen("tcp", t.Local)
+	if err != nil {
+		log.Printf(`开启本地端口监听失败: %s, %s`, t.Local, err)
+		return
+	}
+	defer func() {
+		ll.Close()
+		log.Printf(`断开隧道连接：%s <=> %s`, t.Local, t.Remote)
+	}()
+	log.Printf(`开启隧道：%s <=> %s`, t.Local, t.Remote)
+	for {
+		lc, err := ll.Accept()
+		if err != nil {
+			log.Printf(`接受来自本地的连接失败: %s`, err)
+			return
+		}
+		log.Printf(`接收到本地连接 => %s`, t.Local)
+		sc, err := st.Client()
+		if err != nil {
+			log.Printf(`连接到服务器失败: %s`, err)
+			return
+		}
+		rc, err := sc.Dial("tcp", t.Remote)
+		if err != nil {
+			log.Printf(`连接到远程主机失败: %s`, err)
+			return
+		}
+		log.Printf(`连接到远程主机 => %s `, t.Remote)
+		go transfer(lc, rc)
+	}
+}
+
+func main() {
+	var sts []SSHTunnel
 	if len(os.Args) == 1 {
-		log.Println(`请输入配置文件路径参数.`)
+		log.Println(`缺少配置文件路径参数`)
 		return
 	}
 	p := os.Args[1]
@@ -44,9 +105,6 @@ func init() {
 		log.Printf(`解析配置文件内容出错: %s`, err)
 		os.Exit(-1)
 	}
-}
-
-func main() {
 	var wg sync.WaitGroup
 	for _, st := range sts {
 		wg.Add(1)
@@ -54,101 +112,33 @@ func main() {
 			start(st)
 			wg.Done()
 		}()
+		log.Printf(`启动隧道配置：%s`, st.Addr)
 	}
 	wg.Wait()
 }
 
 func start(st SSHTunnel) {
-	var auth []ssh.AuthMethod
-	auth = make([]ssh.AuthMethod, 0)
-	auth = append(auth, ssh.Password(st.Pass))
-
-	sc := &ssh.ClientConfig{
-		User: st.User,
-		Auth: auth,
-		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
-			return nil
-		},
-	}
-	scon, err := ssh.Dial("tcp", st.Addr, sc)
-	if err != nil {
-		log.Printf(`连接SSH服务器失败: %s`, err)
-		return
-	}
-	defer scon.Close()
-
-	log.Printf("连接到SSH服务器成功: %s", st.Addr)
-
-	go keepalive(scon)
-
+	defer st.Close()
 	var wg sync.WaitGroup
 	for _, t := range st.Tunnels {
 		wg.Add(1)
 		go func(tunnel Tunnel) {
-			connect(scon, tunnel)
+			st.connect(tunnel)
 			wg.Done()
 		}(t)
 	}
 	wg.Wait()
 }
 
-func connect(scon *ssh.Client, t Tunnel) {
-	fmt.Println(t.Remote, t.Local)
-
-	ll, err := net.Listen("tcp", t.Local)
-	if err != nil {
-		log.Printf(`开启本地TCP端口监听失败: %s`, err)
-		return
-	}
-	sno := 0
-	for {
-		lc, err := ll.Accept()
-		if err != nil {
-			log.Printf(`接受来自本地的连接失败: %s`, err)
-			return
-		}
-		sno = sno + 1
-		cid := fmt.Sprintf("%s <=> %s: %d", t.Local, t.Remote, sno)
-		log.Printf(`接受来自本地的连接：%s `, cid)
-		rc, err := scon.Dial("tcp", t.Remote)
-		if err != nil {
-			log.Printf(`连接远程服务器失败: %s`, err)
-			return
-		}
-		go transfer(cid, lc, rc)
-	}
-}
-
-func transfer(cid string, lc net.Conn, rc net.Conn) {
+func transfer(lc net.Conn, rc net.Conn) {
 	go func() {
+		defer lc.Close()
+		defer rc.Close()
 		io.Copy(rc, lc)
-		log.Printf(`断开上行通道：%s`, cid)
-		lc.Close()
-		rc.Close()
 	}()
 	go func() {
+		defer rc.Close()
+		defer lc.Close()
 		io.Copy(lc, rc)
-		log.Printf(`断开下行通道：%s`, cid)
-		rc.Close()
-		lc.Close()
 	}()
-}
-
-func keepalive(scon *ssh.Client) {
-	ss, err := scon.NewSession()
-	if err != nil {
-		log.Printf(`开启SSH会话失败: %s`, err)
-		return
-	}
-	defer ss.Close()
-	w, err := ss.StdinPipe()
-	if err != nil {
-		log.Println("建立SSH会话通道出错", err)
-		return
-	}
-	defer w.Close()
-	for {
-		w.Write([]byte("\n"))
-		time.Sleep(5 * time.Minute)
-	}
 }
