d59cfea97661b711ce10f478c21a130881061b74
chenyong <scchenyong@189.cn>
2020-10-08 14:47:37 +0800

    调整打包命令

--- version/linux64.bat
+++ version/linux64.bat
@@ -1,6 +1,6 @@
 set GO111MODULE=on
 set GOOS=linux
 set GOARCH=amd64
-go build -ldflags "-w -s" sshtunnel main.go
+go build -ldflags "-w -s" -o sshtunnel_linux_amd64 main.go
 
 pause
--- version/win64.bat
+++ version/win64.bat
@@ -1,6 +1,6 @@
 set GO111MODULE=on
 set GOOS=windows
 set GOARCH=amd64
-go build -ldflags "-w -s" -o sshtunnel.exe main.go
+go build -ldflags "-w -s" -o sshtunnel_windows_amd64.exe main.go
 
 pause
